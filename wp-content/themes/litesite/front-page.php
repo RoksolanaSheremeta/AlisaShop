0a2e66d353b625a7064c77cdd638129a31da9dcf
RoksolanaSheremeta <39887580+RoksolanaSheremeta@users.noreply.github.com>
2018-06-18 18:10:15 +0300

    add header add quality

--- wp-content/themes/litesite/front-page.php
+++ wp-content/themes/litesite/front-page.php
@@ -8,6 +8,27 @@
 ?>
 <?php get_header(); ?>
 
-
+<section class="posts_quality">
+		<div class="container">
+			<div class="row">
+				<?php 
+				if( have_rows('fp_advantages') ):
+					while ( have_rows('fp_advantages') ) : the_row();?>
+						<div class="col-xl-4">
+							<div class="post_quality post_quality_row">
+								<img src="<?php the_sub_field('fp_advantages_img'); ?>" alt="quality_2">
+								<div class="wrap">
+									<div class="title"><?php the_sub_field('fp_advantages_title'); ?></div>
+									<div class="description"><?php the_sub_field('fp_advantages_description'); ?></div>
+								</div>
+							</div>
+						</div>
+						<?php
+					endwhile;
+				endif;
+				?>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
