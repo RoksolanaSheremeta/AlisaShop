8a2bf988d3a4afae279749dad66c60617a024dbf
RoksolanaSheremeta <39887580+RoksolanaSheremeta@users.noreply.github.com>
2018-06-22 14:54:38 +0300

    add template_contact

--- wp-content/themes/litesite/template-contact.php
+++ wp-content/themes/litesite/template-contact.php
@@ -3,33 +3,28 @@
 
 <div class="tpl_contact">
 	<div class="container">
-		<?php the_title(); ?>
+		<p class="title-p"><?php the_title(); ?></p>
 		<div class="row">
+			<div class="main_contant">
+				<p class="sub-title"><?php the_field('t_contact_title_left'); ?></p>
+				<?php 
+				if( have_rows('f_contacts', 'options') ):
+					while ( have_rows('f_contacts' ,'options') ) : the_row();?>
+						<a href="<?php the_sub_field('f_phones_or_emails'); ?><?php the_sub_field('f_phone_or_email_link'); ?>"><?php the_sub_field('f_phone_or_email'); ?></a>
+						<?php
+					endwhile;
+				endif;
+				?>
+				<?php the_field('t_contact_info_left'); ?>				
+			</div>
+			<div class="contact_form">
+				<p class="sub-title"><?php the_field('t_contact_title_right'); ?></p>
 
-<?php the_field('t_contact_title_left'); ?>
-<?php 
-if( have_rows('f_contacts', 'options') ):
-	while ( have_rows('f_contacts' ,'options') ) : the_row();?>
-		<a href="<?php the_sub_field('f_phones_or_emails'); ?><?php the_sub_field('f_phone_or_email_link'); ?>"><?php the_sub_field('f_phone_or_email'); ?></a>
-		<?php
-	endwhile;
-endif;
-?>
-<?php the_field('t_contact_info_left'); ?>
-
-
-
-
-
-
-
-<?php the_field('t_contact_title_right'); ?>
-
-<?php 
-$form_shortcode = get_field('t_contact_form_right'); 
-echo do_shortcode($form_shortcode);
-?>
-
+				<?php 
+				$form_shortcode = get_field('t_contact_form_right'); 
+				echo do_shortcode($form_shortcode);
+				?>				
+			</div>
 		</div>
 	</div>
 </div>
